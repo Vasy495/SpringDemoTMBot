f9c64dcf371bc94186791242b6432d41708d55c6
tinkov <vasy499@mail.ru>
2023-04-16 11:16:37 +0300

    Initial commit

---
+++ src/main/java/com/mytinkov/springdemotmbot/SpringDemoTmBotApplication.java
@@ -0,0 +1,13 @@
+package com.mytinkov.springdemotmbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoTmBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemoTmBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mytinkov/springdemotmbot/SpringDemoTmBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mytinkov.springdemotmbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoTmBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
