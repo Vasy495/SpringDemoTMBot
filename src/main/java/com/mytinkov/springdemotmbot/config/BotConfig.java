2c4a9d08b504d39b61cc1fd8fcd0d8288a7a8cb1
tinkov <vasy499@mail.ru>
2023-04-17 15:12:09 +0300

    logging done commit

--- src/main/java/com/mytinkov/springdemotmbot/config/BotConfig.java
+++ src/main/java/com/mytinkov/springdemotmbot/config/BotConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@Data //Автоматически создает конструкторы для класса
+@Data
 @PropertySource("application.properties")
 public class BotConfig {
     @Value("${bot.name}")
